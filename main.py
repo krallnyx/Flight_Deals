68efda227295313aa6727891fa268854b9fe87cc
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-03-05 15:43:44 +0000

    #0 Empty project

---
+++ main.py
@@ -0,0 +1,9 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+class FlightDeals:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    flight_deals = FlightDeals()
